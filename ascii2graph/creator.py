71b551ba7ac6a359e9aac2961298f5aaa4cf8243
Ferry Jérémie <ferryjeremie@free.fr>
2015-01-26 23:09:39 +0100

    diagram + inner text

--- ascii2graph/creator.py
+++ ascii2graph/creator.py
@@ -3,6 +3,56 @@ import svgwrite
 from PIL import Image
 
 
+class Style:
+    fill = 'white'
+    stroke = 'black'
+
+
+class DiagramCreator:
+    style = Style()
+    group = None
+
+    def __init__(self, size, dwg, diagrams):
+        self.size = size
+        self.dwg = dwg
+        self.diagrams = diagrams
+
+    def create(self):
+        for diagram in self.diagrams:
+            self._create_diagram(diagram)
+
+            texts = diagram.text.get()
+
+            # vertical align when addition of padding-top and padding-bottom are not pair
+            vertical_align = 0
+            if ((diagram.height - len(texts)) % 2 == 1):
+                vertical_align = self.size / 2
+            for text in texts:
+                self.group.add(self.dwg.text(
+                    text=texts[text][1].strip(),
+                    dx=[texts[text][0] * self.size],
+                    dy=[text * self.size * 2 + vertical_align],
+                    textLength=len(texts[text][1].strip()) * self.size
+                ))
+
+    def _create_diagram(self, diagram):
+        self.group = self.dwg.add(self.dwg.g(
+            transform='translate(%s, %s)' % (
+                self.size * diagram.x,
+                2 * self.size * diagram.y
+            )
+        ))
+        self.group.add(self.dwg.rect(
+            (0, 0),
+            (self.size * diagram.width, 2 * self.size * diagram.height),
+            fill=self.style.fill, stroke=self.style.stroke)
+        )
+
+
+class OvaleDiagramCreator(DiagramCreator):
+    pass
+
+
 class SVGPicture:
     def __init__(self, path=None):
         self.path = path
@@ -13,27 +63,15 @@ class SVGPicture:
         )
         self.dwg.add_stylesheet('static/style.css', title='style_css')
         #self.dwg.container.Style(content='font-size: 5px;')
+
         # Diagrams
-        for diagram in ascii_parser.diagrams:
-            group = self.dwg.add(self.dwg.g(
-                transform='translate(%s, %s)' % (
-                    size * diagram.x,
-                    2 * size * diagram.y
-                )
-            ))
-            group.add(self.dwg.rect(
-                (0, 0),
-                (size * diagram.width, 2 * size * diagram.height),
-                fill='white', stroke='black')
-            )
-            texts = diagram.text.get()
-            for text in texts:
-                group.add(self.dwg.text(
-                    text=texts[text][1],
-                    dx=[texts[text][0] * size ],
-                    dy=[text * size * 2],
-                    textLength=len(texts[text][1]) * size
-                ))
+        diagramCreator = DiagramCreator(size, self.dwg, ascii_parser.diagrams)
+        diagramCreator.create()
+
+        # Ovale Diagrams
+        ovaleDiagramCreator = OvaleDiagramCreator(size, self.dwg, ascii_parser.ovale_diagrams)
+        ovaleDiagramCreator.create()
+
         # Rectangles
         for rectangle in ascii_parser.rectangles:
             self.dwg.add(self.dwg.rect(
@@ -59,7 +97,6 @@ class PictureCreator:
             self.output_picture = SVGPicture(output_file)
         else:
             self.output_picture = BitmapPicture(output_file)
-
 
     def create(self, ascii_parser=None, size=1):
         self.output_picture.create(ascii_parser, size)
--- ascii2graph/parser.py
+++ ascii2graph/parser.py
@@ -1,3 +1,10 @@
+class Corners:
+    top_left = '+'
+    top_right = '+'
+    bottom_left = '+'
+    bottom_right = '+'
+
+
 class Rectangle:
     def __init__(self, x1, x2, y1, y2):
         self.x = x1
@@ -9,6 +16,8 @@ class Rectangle:
 
 
 class TextDiagram:
+    left_space = True
+
     def __init__(self):
         self._text = {}
 
@@ -21,8 +30,14 @@ class TextDiagram:
 
     def add(self, x=0, y=0, value=''):
         if y in self._text:
-            self._text[y][1] += value
+            # left position
+            if (value == " " and self.left_space):
+                self._text[y][0] = x
+            else:
+                self.left_space = False
+                self._text[y][1] += value
         else:
+            self.left_space = True
             self._text[y] = [x, value]
         return True
 
@@ -31,8 +46,14 @@ class Diagram(Rectangle):
     text = None
 
 
+class OvaleDiagram(Diagram):
+    text = None
+
+
 class DiagramCreator:
     def __init__(self, grid):
+        self.corners = Corners()
+
         self.diagrams = []
         self.rectangles = []
         self.grid = grid
@@ -50,7 +71,7 @@ class DiagramCreator:
         i = 1
         while True:
             value = self.grid.value(x=point.x + i, y=point.y)
-            if value == '+':
+            if value == self.corners.top_right:
                 point = Point(value, x=point.x + i, y=point.y)
                 break
             if value != '-':
@@ -60,7 +81,7 @@ class DiagramCreator:
         i = 1
         while True:
             value = self.grid.value(x=point.x, y=point.y + i)
-            if value == '+':
+            if value == self.corners.bottom_right:
                 point = Point(value, x=point.x, y=point.y + i)
                 x2 = point.x
                 break
@@ -71,7 +92,7 @@ class DiagramCreator:
         i = 1
         while True:
             value = self.grid.value(x=point.x - i, y=point.y)
-            if value == '+':
+            if value == self.corners.bottom_left:
                 point = Point(value, x=point.x - i, y=point.y)
                 y2 = point.y
                 break
@@ -82,14 +103,14 @@ class DiagramCreator:
         i = 1
         while True:
             value = self.grid.value(x=point.x, y=point.y - i)
-            if value == '+':
+            if value == self.corners.top_left:
                 point = Point(value, x=point.x, y=point.y - i)
                 break
             if value != '|':
                 return False
             i += 1
 
-        diagram = self._get_diagram(x1 - 1, y1, x2, y2)
+        diagram = self._get_diagram(x1 - 1, x2, y1, y2)
         if not diagram:
             self.rectangles.append(Rectangle(x1 - 1, x2, y1, y2))
             return True
@@ -97,20 +118,40 @@ class DiagramCreator:
         return True
 
     def _get_diagram(
-        self, init_x=0, init_y=0, end_x=0, end_y=0,
-        pattern=[' ', '-', '|']
+        self, init_x=0, end_x=0, init_y=0, end_y=0,
+        pattern=['-', '|']
     ):
         text = TextDiagram()
         for y in range(init_y + 1, end_y):
-            for x in range(init_x, end_x):
+            for x in range(init_x + 1, end_x):
                 value = self.grid.value(x, y)
                 if value not in pattern:
-                    text.add(x=x - init_x - 1, y=y - init_y, value=value)
+                    text.add(x=x - init_x, y=y - init_y, value=value)
         if not text.get():
             return False
-        diagram = Diagram(init_x, end_x, init_y, end_y)
+        diagram = OvaleDiagram(init_x, end_x, init_y, end_y)
         diagram.text = text
         return diagram
+
+
+class OvaleDiagramCreator(DiagramCreator):
+    def __init__(self, grid):
+        self.corners = Corners()
+        self.corners.top_left = '/'
+        self.corners.top_right = '\\'
+        self.corners.bottom_left = '\\'
+        self.corners.bottom_right = '/'
+
+        self.diagrams = []
+        self.rectangles = []
+        self.grid = grid
+        for point in self.points:
+            new_rectangle = self._create(point)
+
+    @property
+    def points(self):
+        yield from self.grid.search('/')
+        yield from self.grid.search('\\')
 
 
 class Point:
@@ -119,6 +160,11 @@ class Point:
         self.x = x
         self.y = y
 
+    def __str__(self):
+        return 'value: "{0}" x: "{1}" y: "{2}"'.format(
+            self.value, self.x, self.y
+        )
+
 
 class Grid:
     max_x = 0
@@ -126,7 +172,6 @@ class Grid:
 
     def __init__(self, lines):
         self._array = {}
-        self.max_y = len(lines)
 
         for line_nb, line in enumerate(lines):
             line = line.rstrip()
@@ -136,6 +181,8 @@ class Grid:
                 self.max_x = char_len
             for char_nb, character in enumerate(line):
                 self._array[line_nb][char_nb] = character
+
+        self.max_y = len(lines)
 
         for y in range(0, self.max_y):
             if y not in self._array:
@@ -149,14 +196,14 @@ class Grid:
         for y in range(0, self.max_y):
             for x in range(0, self.max_x):
                 stream += self._array[y][x]
-            if y != self.max_y - 1:
+            if y != self.max_y:
                 stream += '\n'
         return stream
 
     def value(self, x=0, y=0):
         if x > self.max_x:
             return False
-        if y > self.max_y:
+        if y >= self.max_y:
             return False
         return self._array[y][x]
 
@@ -171,10 +218,12 @@ class AsciiParser:
     def __init__(self, lines):
         self.rectangles = []
         grid = Grid(lines)
-        # print('-----')
-        # print(grid)
-        # print('-----')
-        diagram_creator = DiagramCreator(grid)
+        #print(grid)
+        #print(grid._array)
 
+        diagram_creator = DiagramCreator(grid)
+        ovale_diagram_creator = OvaleDiagramCreator(grid)
+
+        self.ovale_diagrams = (diagram for diagram in ovale_diagram_creator.diagrams)
         self.diagrams = (diagram for diagram in diagram_creator.diagrams)
         self.rectangles = (rectangle for rectangle in diagram_creator.rectangles)
